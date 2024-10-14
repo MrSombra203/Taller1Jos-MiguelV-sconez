7aceb6fc2e1a4788e3a4ffe378c871e75de94fdf
José Miguel Vásconez Andrade <jose.vasconez.andrade@udla.edu.ec>
2024-10-14 13:34:26 -0500

    Se hagrego los modelos

---
+++ Taller1JoséMiguelVásconez/Models/Equipo.cs
@@ -0,0 +1,12 @@
+﻿namespace Taller1JoséMiguelVásconez.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ciudad {  get; set; }
+        public int Titulos { get; set; }
+        public bool AceptaExtranjeros {  get; set; }
+        public Estadio Estadio { get; set; }
+    }
+}
---
+++ Taller1JoséMiguelVásconez/Models/Jugador.cs
@@ -0,0 +1,11 @@
+﻿namespace Taller1JoséMiguelVásconez.Models
+{
+    public class Jugador
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Pocición {  get; set; }
+        public int Edad {  get; set; }
+        public Equipo Equipo { get; set; }
+    }
+}
